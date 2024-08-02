dec047d90b8a1bd5e4bbca2a66e7c652e7844baa
Esteban Ochoa <estebanochoa@protonmail.com>
2024-08-02 12:38:07 -0500

    Missing item

--- lib/helix_web/controllers/main_controller.ex
+++ lib/helix_web/controllers/main_controller.ex
@@ -7,15 +7,4 @@ defmodule HelixWeb.MainController do
     conn |> put_status(200) |> json(%{status: "success", data: "OK"})
   end
 
-  defp validate_security_token(conn) do
-    token = Enum.at(get_req_header(conn, "authorization"), 0)
-    case TokenManager.validate_token(token) do
-      {:ok, username } -> case username do
-                            "eventtia-cartier" -> true
-                            _ -> false
-                          end
-      {:error, _} -> false
-    end
-  end
-
 end
